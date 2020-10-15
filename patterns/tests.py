5a93b322fd9b2176143b291773c74d5d60f8b658
becky112358 <8b2@protonmail.com>
2020-10-15 16:45:12 +0300

    Add a test to ensure that a validation error is raised if the user attempts to create a pattern with a siteswap with an invalid character.

--- patterns/tests.py
+++ patterns/tests.py
@@ -11,6 +11,14 @@ class PatternModelTests(TestCase):
         A validation error is raised if the user attempts to create a pattern
         with an invalid siteswap.
         """
-        self.assertRaises(ValidationError, Pattern(siteswap='1234', prop_type=Pattern.PropType.BALL).full_clean)
+
+        invalid_siteswaps = [
+            '1&2&3',
+            '1234',
+        ]
+
+        for i in range(len(invalid_siteswaps)):
+            self.assertRaises(ValidationError,
+                              Pattern(siteswap=invalid_siteswaps[i], prop_type=Pattern.PropType.BALL).full_clean)
 
 
