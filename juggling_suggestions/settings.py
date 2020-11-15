66946b1e7abec8bbdf990db90e32067662b0e352
becky112358 <8b2@protonmail.com>
2020-11-15 21:08:58 +0300

    Add code for the situation where the secrets file does not exist, in order to make git collaboration easier.

--- juggling_suggestions/settings.py
+++ juggling_suggestions/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-from .secrets import secret_key
+try:
+    from .secrets import secret_key
+except ModuleNotFoundError:
+    secret_key = "It's a secret!"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
